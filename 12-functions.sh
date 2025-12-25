4294161b049b8aaac4a92ca6a69e9133f8c428c7
avk-git-07 <asurivamshi@gmail.com>
2025-12-25 17:44:21 +0530

    2nd commit of 12-functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -14,16 +14,16 @@ VALIDATE() {
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run the script with the root user"
+    echo "\e[32m Please run the script with the root user \e[0m"
     exit 1
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed, so we are installing it now.."
+    echo "\e[32m MySQL is not installed, so we are installing it now.. \e[0m"
     dnf install mysql -y
     VALIDATE $? "MySQL"
 else
-    echo "MySQL is already installed, nothing to do !!!"
+    echo "\e[32m MySQL is already installed, nothing to do !!! \e[0m"
 fi
