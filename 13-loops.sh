f10bd518941e0b201226b7a9882afc669e1f1701
avk-git-07 <asurivamshi@gmail.com>
2025-12-26 09:52:15 +0530

    Loops 1st commit

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
 for i in {0..10}
 do 
     echo $i
 done
+
+if [ $USERID -ne 0 ]
+then
+    echo "$R Run this script with root user $N"
+    exit 1
+fi
